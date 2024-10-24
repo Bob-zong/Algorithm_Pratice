eb3d77858211f03f79d0f5c5d2a6d701f91be165
Park Jun Yong <wns0882@naver.com>
2024-10-24 16:36:13 +0900

    Title: 정수만 추출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241024/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        int len1 = str1.length();
+        int len2 = str1.length();
+
+        for(int i = 0; i<len1; i++){
+            if('0' > str1.charAt(i) || str1.charAt(i) >'9'){
+                str1 = str1.substring(0,i);
+                break;
+            }
+        }
+        for(int i = 0; i<len2; i++){
+            if('0' > str2.charAt(i) || str2.charAt(i) >'9'){
+                str2 = str2.substring(0,i);
+                break;
+            }
+        }
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+        
+        System.out.print(num1 + num2);
+
+    }
+}
