9b76e2facd67b9b8d482ac3a35976f63f5219791
Park Jun Yong <wns0882@naver.com>
2024-10-24 16:22:17 +0900

    Title: 대문자 소문자 바꾸기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241024/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int len = str.length();
+
+        for(int i = 0; i < len; i++){
+            if('a' <= str.charAt(i) && str.charAt(i) <= 'z'){
+                System.out.print((char)(str.charAt(i)-'a'+'A'));
+            }
+            if('A' <= str.charAt(i) && str.charAt(i) <= 'Z'){
+                System.out.print((char)(str.charAt(i)-'A'+'a'));
+            }
+            
+        }
+    }
+}
