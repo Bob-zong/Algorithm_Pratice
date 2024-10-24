0e20d4f477b5da48315b71972ba1b80eed370318
Park Jun Yong <wns0882@naver.com>
2024-10-24 16:48:12 +0900

    Title: 정수만 추출하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/정수만 추출하기/extract-only-integers.java
+++ 241024/정수만 추출하기/extract-only-integers.java
@@ -7,17 +7,20 @@ public class Main {
         String str1 = sc.next();
         String str2 = sc.next();
         int len1 = str1.length();
-        int len2 = str1.length();
+        int len2 = str2.length();
 
         for(int i = 0; i<len1; i++){
             if('0' > str1.charAt(i) || str1.charAt(i) >'9'){
                 str1 = str1.substring(0,i);
+                // System.out.print(str1 + " ");
                 break;
             }
         }
+
         for(int i = 0; i<len2; i++){
             if('0' > str2.charAt(i) || str2.charAt(i) >'9'){
                 str2 = str2.substring(0,i);
+                // System.out.print(str2);
                 break;
             }
         }
