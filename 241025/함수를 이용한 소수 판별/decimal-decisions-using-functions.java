0a7c6ba0254a6150f5ba4a27c33acdde1c9a3dae
Park Jun Yong <wns0882@naver.com>
2024-10-25 17:29:05 +0900

    Title: 함수를 이용한 소수 판별, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 241025/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -20,6 +20,8 @@ public class Main {
         return sum;
     }
     public static boolean isPrime(int num){
+        if(num == 1)
+            return false;
         for(int i = 2; i < num; i++){
             if(num % i == 0){
                 return false;
