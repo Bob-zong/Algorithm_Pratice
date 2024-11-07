b79b9c8643b038b52915858b7945344c23980023
Park Jun Yong <wns0882@naver.com>
2024-11-07 15:35:08 +0900

    Title: 그 계절, 그 날, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241107/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int year, month, day;
+        year = sc.nextInt();
+        month = sc.nextInt();
+        day = sc.nextInt();
+        boolean satisfied;
+        satisfied = isDay(year,month,day);
+        if(satisfied){
+            System.out.print(printWeather(month));
+        }
+        else{
+            System.out.print(-1);
+        }
+    }
+    public static boolean isDay(int year, int month,int day){
+        if(month == 4 || month == 6 || month == 9 || month == 11)
+            if(day <= 30)
+                return true;
+            else
+                return false;
+        else if(month == 2){
+            if(isYear(year))
+                return true;
+            else{
+                return false;
+            } 
+        }else{
+            return true;
+        }
+        
+    }
+
+    public static boolean isYear(int year){
+        if(year % 4 != 0){
+             return false;
+        }
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false; 
+    }
+    public static String printWeather(int month){
+        String strWeather;
+        if(month >= 3 && month <= 5){
+            strWeather = "Spring";
+            return strWeather;
+        }
+        else if(month >= 6 && month <= 8){
+            strWeather = "Summer";
+            return strWeather;
+        }
+        else if(month >= 9 && month <= 11){
+            strWeather = "Fall";
+            return strWeather;
+        }
+        else{
+            strWeather = "Winter";
+            return strWeather;
+        }
+            
+    }
+}
