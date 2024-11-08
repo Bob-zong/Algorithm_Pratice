401d4e79c648d8d7011b06b42f5c5a45d99a7251
Park Jun Yong <wns0882@naver.com>
2024-11-08 16:06:47 +0900

    Title: 짝수만 2로 나누기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241108/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void modify(int[] arr2){
+        for(int i = 0; i < arr2.length; i++){
+            if(arr2[i] % 2 == 0){
+                arr2[i] = (arr2[i] / 2);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        modify(arr);
+        
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
