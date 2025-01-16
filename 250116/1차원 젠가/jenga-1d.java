2fb237223ff374f87d7bfdb0c776c981139f74d4
Park Jun Yong <wns0882@naver.com>
2025-01-16 10:03:44 +0900

    Title: 1차원 젠가, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

--- 250116/1차원 젠가/jenga-1d.java
+++ 250116/1차원 젠가/jenga-1d.java
@@ -2,50 +2,49 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n;
+    public static int[] answer;
+    public static int endOfArray;
+
+    public static void cutArray(int startIdx, int endIdx){
+        int[] tempArr = new int[n];
+        int endOfTempArray = 0;
+
+        for(int i = 0; i < endOfArray; i++){
+            if( i < startIdx || i > endIdx)
+                tempArr[endOfTempArray++] = answer[i];
+        }
+        
+        for(int i = 0; i < endOfTempArray; i++)
+            answer[i] = tempArr[i];
+
+        endOfArray = endOfTempArray;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        final int BLANK = 0;
 
-        int n = Integer.parseInt(br.readLine());
-        int[] answer = new int[n+1];
-        int[] tempArr = new int[n+1];
+        n = Integer.parseInt(br.readLine());
+        answer = new int[n];
+        endOfArray = n;
 
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
             answer[i] = Integer.parseInt(br.readLine());
         }
         
-        int endOfTempArray = 1;
-
-        for(int i = 0; i < 2; i++){
+        for(int k = 0; k < 2; k++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            for(int row = 1; row <= n ; row++){
-                if( row < start || row > end){
-                    tempArr[endOfTempArray] = answer[row];
-                    // System.out.print(tempArr[endOfTempArray]);
-                    endOfTempArray++;
-                }        
-            }
-            // System.out.println();
-            answer = tempArr.clone();
-            // for(int k = 1; k <= n; k++){
-            //     System.out.print(answer[k] + " ");
-            // }
-            endOfTempArray = 1;
+            start--; end--;
+            cutArray(start, end);
         }
-        // System.out.println();
-        int cnt = 0;
-        for(int i = 1; i <= n; i++){
-            if(answer[i] != BLANK)
-                cnt++;
-        }
-        System.out.println(cnt);
-        for(int i = 1; i <= n; i++){
-            if(answer[i] != BLANK)
-                System.out.println(answer[i]);
-        }
+
+        System.out.println(endOfArray);
+        for(int i = 0; i < endOfArray; i++)
+            System.out.println(answer[i]);
+        
     }
 }
