7f137392981b50a8b75667a5f16d16fc9a38b6fd
Park Jun Yong <wns0882@naver.com>
2025-01-21 08:58:45 +0900

    Title: 계단 오르기, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

--- 250121/계단 오르기/climbing-stairs.java
+++ 250121/계단 오르기/climbing-stairs.java
@@ -4,26 +4,29 @@ import java.io.*;
 public class Main {
     public static int n;
     public static final int MAX_NUM = 1000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int[] dp = new int[MAX_NUM + 1];
+        // n -> 2 * a + 3 * b 가 될 때 만족 dp[]
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[MAX_NUM+1];
 
+        dp[0] = 1;
+        dp[1] = 0;
         dp[2] = 1;
         dp[3] = 1;
 
-        // n -> 2 * a + 3 * b 가 될 때 만족 dp[]
-        int n = Integer.parseInt(br.readLine());
-
         for(int i = 4; i <= n; i++){
-            dp[i] = dp[i -2] + dp[i-3];
+            dp[i] = (dp[i-2] + dp[i-3]) % 10007;
         }
-        if(dp[n] != 0)
-            System.out.print(dp[n] % 10007);
-        else
-            System.out.print(0);
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+        
     }
 }
