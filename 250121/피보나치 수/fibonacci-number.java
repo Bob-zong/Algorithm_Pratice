81b308594256981eaac549512f68c47a71a8a428
Park Jun Yong <wns0882@naver.com>
2025-01-21 07:38:16 +0900

    Title: 피보나치 수, Time: 79ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250121/피보나치 수/fibonacci-number.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static int dp[];
+    public static int memo[];
+
+    public static int Fibbo(int n){
+        if(memo[n] != -1)
+            return memo[n];
+        if(n <= 2)
+            memo[n] = 1;
+        else{
+            memo[n] = Fibbo(n-1) + Fibbo(n-2);
+        }
+        return memo[n];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        dp = new int[46];
+        int n = Integer.parseInt(br.readLine());
+        memo = new int[46];
+        Arrays.fill(memo, -1);
+
+        int ans = Fibbo(n);
+        System.out.print(ans);
+
+        
+    }
+}
