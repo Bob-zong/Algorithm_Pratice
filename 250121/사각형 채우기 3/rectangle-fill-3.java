3dad38884d4a4411200da1d5e3b7935170f42ee8
Park Jun Yong <wns0882@naver.com>
2025-01-21 11:20:16 +0900

    Title: 사각형 채우기 3, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 250121/사각형 채우기 3/rectangle-fill-3.java
+++ 250121/사각형 채우기 3/rectangle-fill-3.java
@@ -9,13 +9,15 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[MAX_NUM + 1];
+        long[] dp = new long[MAX_NUM + 1];
         dp[0] = 1;
         dp[1] = 2;
         dp[2] = 7;
 
         for(int i = 3; i <= n; i++){
-            dp[i] = (dp[i-1] * 2 + dp[i-2] * 4) % 1000000007;
+            dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % 1000000007;
+            for(int j = i - 3; j >= 0; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % 1000000007;
         }
         System.out.print(dp[n]);
     }
