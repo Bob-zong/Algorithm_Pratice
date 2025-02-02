c12899a5244750a51023672e2f275165e136c395
Park Jun Yong <wns0882@naver.com>
2025-02-03 08:41:00 +0900

    Title: 거꾸로 순열, Time: 89ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 250203/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static StringBuilder sb;
+    public static boolean[] visited;
+    public static List<Integer> answer = new ArrayList<>();
+
+    public static void print(){
+        for(int i = 0; i < n; i++){
+            sb.append(answer.get(i) + " ");
+        }
+        sb.append("\n");
+    }
+
+    public static void choose(int currNum){
+        if(currNum == 0){
+            print();
+            return;
+        }
+
+        for(int i = 3; i >= 1; i--){
+            if(visited[i])
+                continue;
+
+            answer.add(i);
+            visited[i] = true;
+            choose(currNum - 1);
+            answer.remove(answer.size()-1);
+            visited[i] = false;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n+1];
+
+        choose(3);
+
+        System.out.print(sb);
+
+    }
+}   
