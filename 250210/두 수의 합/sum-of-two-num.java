26213127d4ec1f80afac2891510436c812305768
Park Jun Yong <wns0882@naver.com>
2025-02-10 16:21:16 +0900

    Title: 두 수의 합, Time: 346ms, Memory: 23MB, Status: Passed - CodeTree

--- 250210/두 수의 합/sum-of-two-num.java
+++ 250210/두 수의 합/sum-of-two-num.java
@@ -2,38 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, k;
-    public static Map<Integer, Integer> map;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        map = new HashMap<>();
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        int cnt = 0;
-
-        for(int i = 0; i < n; i++){
-            int num = Integer.parseInt(st.nextToken());
-            map.put(i, num);
-        }
-
-        for(int i = 0; i < n-1; i++){
-            for(int j = i+1; j < n; j++){
-                int num1 = map.get(i);
-                int num2 = map.get(j);
-                if(num1 + num2 == k){
-                    cnt += 1;
-                }
-            }
-        }
-
-        System.out.print(cnt);
-
-
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         int n = Integer.parseInt(st.nextToken());
+         int k = Integer.parseInt(st.nextToken());
+         st = new StringTokenizer(br.readLine());
+         
+         // 각 숫자의 등장 횟수를 기록하는 맵
+         Map<Integer, Integer> freq = new HashMap<>();
+         
+         for (int i = 0; i < n; i++) {
+             int num = Integer.parseInt(st.nextToken());
+             freq.put(num, freq.getOrDefault(num, 0) + 1);
+         }
+         
+         long count = 0;
+         // 맵의 각 entry를 순회하면서 두 숫자의 합이 k가 되는 경우의 수를 계산
+         for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+             int num = entry.getKey();
+             int frequency = entry.getValue();
+             int complement = k - num;
+             
+             if (!freq.containsKey(complement))
+                 continue;
+             
+             if (num == complement) {
+                 // 같은 숫자인 경우, 조합 공식 적용: frequencyC2
+                 count += (long) frequency * (frequency - 1) / 2;
+             } else if (num < complement) {
+                 // 중복 계산 방지를 위해 num < complement인 경우에만 계산
+                 count += (long) frequency * freq.get(complement);
+             }
+         }
+         
+         System.out.println(count);
     }
 }
