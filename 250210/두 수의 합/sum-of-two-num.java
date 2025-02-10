16702e4bee086c86224902280fbcc3f661110b92
Park Jun Yong <wns0882@naver.com>
2025-02-10 16:14:47 +0900

    Title: 두 수의 합, Time: 1414ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 250210/두 수의 합/sum-of-two-num.java
+++ 250210/두 수의 합/sum-of-two-num.java
@@ -3,66 +3,34 @@ import java.io.*;
 
 public class Main {
     public static int n, k;
-    public static boolean[] visited;
-    public static List<Integer> arrList = new ArrayList<>();
-    public static int cnt;
-    public static int[] answer;
-    public static int print(){
-        int sum = 0;
-
-        for(int i = 0; i < arrList.size(); i++){
-            sum += arrList.get(i);
-        }
-        
-        return sum;
-    }
-
-    public static void choose(int currNum, int idx){
-        if(currNum == 2){
-            int num = print();
-            // System.out.println("value: " + num);
-            if(num == k){
-                cnt += 1;
-            }
-            return;
-        }
-
-        for(int i = idx; i < n; i++){
-            if(visited[i])
-                continue;
-
-            arrList.add(answer[i]);
-            visited[i] = true;
-            choose(currNum + 1, i+1);
-            arrList.remove(arrList.size()-1);
-            visited[i] = false;
-        }
-
-    }
+    public static Map<Integer, Integer> map;
 
     public static void main(String[] args) throws IOException {
-        // String.valueOf(str) -> 문자열을 정수로
-        // Integer.toString(num) -> 정수를 문자열로
-        
-        
-        cnt = 0;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        map = new HashMap<>();
 
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        answer = new int[n];
-        visited = new boolean[n];
-
         st = new StringTokenizer(br.readLine());
+        int cnt = 0;
 
         for(int i = 0; i < n; i++){
-            answer[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            map.put(i, num);
         }
-        
-        choose(0, 0);
+
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                int num1 = map.get(i);
+                int num2 = map.get(j);
+                if(num1 + num2 == k){
+                    cnt += 1;
+                }
+            }
+        }
 
         System.out.print(cnt);
 
