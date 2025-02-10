38151542b069ca1717650787092e82e8e69e2161
Park Jun Yong <wns0882@naver.com>
2025-02-11 08:25:18 +0900

    Title: 자주 등장한 top K 숫자, Time: 791ms, Memory: 26MB, Status: Passed - CodeTree

--- 250211/자주 등장한 top K 숫자/top-k-frequent-elements.java
+++ 250211/자주 등장한 top K 숫자/top-k-frequent-elements.java
@@ -2,36 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, k;
-    public static int[] numArr;
     public static void main(String[] args) throws IOException {
+        // 입력 처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
         Map<Integer, Integer> map = new HashMap<>();
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        numArr = new int[n];
-
+        int[] numArr = new int[n];
         st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i < n; i++){
+        
+        // 배열과 map에 값 저장 (map은 key의 빈도수 기록)
+        for (int i = 0; i < n; i++) {
             numArr[i] = Integer.parseInt(st.nextToken());
-            map.put(numArr[i], map.getOrDefault(numArr[i], 0) + 1); 
+            map.put(numArr[i], map.getOrDefault(numArr[i], 0) + 1);
         }
-
+        
+        // map의 key를 List로 추출
         List<Integer> keySet = new ArrayList<>(map.keySet());
-        keySet.sort(new Comparator<Integer>(){
+        
+        // 내림차순 정렬: 먼저 map의 값(빈도수) 기준 내림차순, 그 후 key 값 기준 내림차순
+        keySet.sort(new Comparator<Integer>() {
             @Override
-            public int compare(Integer key1, Integer key2){
-                return map.get(key2).compareTo(map.get(key1));
+            public int compare(Integer key1, Integer key2) {
+                // 빈도수 내림차순 정렬
+                int cmp = map.get(key2).compareTo(map.get(key1));
+                // 같은 빈도수이면 key값 내림차순 정렬
+                if(cmp == 0) {
+                    return key2.compareTo(key1);
+                }
+                return cmp;
             }
         });
         
-        for(int i = k-1; i >= 0; i--){
+        // keySet의 앞쪽부터 출력 (k개의 값)
+        for (int i = 0; i < k && i < keySet.size(); i++) {
             System.out.print(keySet.get(i) + " ");
         }
-
-
     }
 }
