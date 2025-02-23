776f2c290700d1ec1d46ecb75e0109b3f79ec646
Park Jun Yong <wns0882@naver.com>
2025-02-23 22:35:29 +0900

    Title: 최고의 13위치 2, Time: 83ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250223/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        
+        int[][] answer = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                answer[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int maxCnt = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n-2; j++){
+                for(int k = i+1; k < n; k++){
+                    for(int l = 0; l < n-2; l++){
+                        int cnt = answer[i][j] + answer[i][j+1] + answer[i][j+2] +
+                        answer[k][l] + answer[k][l+1] + answer[k][l+2];
+                        maxCnt = Math.max(maxCnt, cnt);
+                    }
+                }
+            }
+        }
+        System.out.print(maxCnt);
+    }
+}
