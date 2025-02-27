3a1b07d7a86b616c7a68474e4d3e50b4fd64727b
Park Jun Yong <wns0882@naver.com>
2025-02-27 21:57:41 +0900

    Title: DateTime to DateTime, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

--- 250227/DateTime to DateTime/datetime-to-datetime.java
+++ 250227/DateTime to DateTime/datetime-to-datetime.java
@@ -2,6 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int calculate(int hour, int min, int day, int sHour, int sMin, int sDay){
+        int dayDiff = (day - sDay) * 24 * 60;
+        int hourDiff = (hour - sHour) * 60;
+        int minDiff = min - sMin;
+
+        int sum = dayDiff + hourDiff + minDiff;
+        return sum;
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,32 +23,37 @@ public class Main {
         int s_hour = 11;
         int s_min = 11;
 
-        int cnt = 0;
 
-        if(s_day > day)
-            if(s_hour > hour)
-                if(s_min > min){
-                    System.out.print(-1);
-                    System.exit(1);
-                }
+        // int cnt = 0;
 
-        while(!(s_day == day && s_hour == hour && s_min == min)){
-            // if(s_day == day && s_hour == hour && s_min == min)
-            //     break;
+        // if(s_day > day)
+        //     if(s_hour > hour)
+        //         if(s_min > min){
+        //             System.out.print(-1);
+        //             System.exit(1);
+        //         }
 
-            s_min += 1;
-            cnt += 1;
+        // while(!(s_day == day && s_hour == hour && s_min == min)){
+        //     // if(s_day == day && s_hour == hour && s_min == min)
+        //     //     break;
 
-            if(s_min == 60){
-                s_hour += 1;
-                s_min = 0;
-            }
+        //     s_min += 1;
+        //     cnt += 1;
 
-            if(s_hour == 24){
-                s_day += 1;
-                s_hour = 0;
-            }
-        }
-        System.out.print(cnt);
+        //     if(s_min == 60){
+        //         s_hour += 1;
+        //         s_min = 0;
+        //     }
+
+        //     if(s_hour == 24){
+        //         s_day += 1;
+        //         s_hour = 0;
+        //     }
+        // }
+        int ans = calculate(hour, min, day, 11 , 11, 11);
+        if(ans < 0)
+            System.out.print(-1);
+        else
+            System.out.print(ans);
     }
 }
