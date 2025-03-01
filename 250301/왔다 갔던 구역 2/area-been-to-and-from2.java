c6d8193c7265c43790c6cff8613cfff43d972ca9
Park Jun Yong <wns0882@naver.com>
2025-03-01 16:30:47 +0900

    Title: 왔다 갔던 구역 2, Time: 94ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250301/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int OFFSET = 1000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] answer = new int[2001];
+
+        int currPos = 1000;
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int num = Integer.parseInt(st.nextToken());
+            // num += OFFSET;
+            String dir = st.nextToken();
+
+            for(int j = 0; j < num; j++){
+                if(dir.equals("R")){
+                    answer[currPos] += 1;
+                    if(j < num)
+                        currPos += 1;
+                }
+                else{
+                    answer[currPos] += 1;
+                    if(j < num)
+                        currPos -= 1;
+                }
+                
+            }
+        }
+        int cnt = 0;
+
+        for(int i = 0; i < 2001; i++){
+            if(answer[i] > 1)
+                cnt += 1;
+        }
+        System.out.print(cnt);
+    }
+}
