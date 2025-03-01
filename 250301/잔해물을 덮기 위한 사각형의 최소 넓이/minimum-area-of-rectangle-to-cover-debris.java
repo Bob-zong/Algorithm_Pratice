18f11cab88f36f45571d5c0fc105a5b763a89b5c
Park Jun Yong <wns0882@naver.com>
2025-03-01 22:24:36 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250301/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250301/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -4,54 +4,66 @@ import java.io.*;
 
 public class Main {
     public static final int OFFSET = 1000;
+    public static final int MAX_R = 2000;
+    public static final int N = 2;
+    
+    public static int[] x1 = new int[N];
+    public static int[] y1 = new int[N];
+    public static int[] x2 = new int[N];
+    public static int[] y2 = new int[N];
+    public static int[][] checked = new int[MAX_R + 1][MAX_R + 1];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int answer[][] = new int[2001][2001];
-
-        for(int i = 0; i < 2; i++){
+        for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
 
-            int x1, y1, x2, y2;
+            x1[i] = Integer.parseInt(st.nextToken()) + OFFSET;
+            y1[i] = Integer.parseInt(st.nextToken()) + OFFSET;
+            x2[i] = Integer.parseInt(st.nextToken()) + OFFSET;
+            y2[i] = Integer.parseInt(st.nextToken()) + OFFSET;
+        }
+        
+        for(int i = 0; i < N; i++)
+            for(int x = x1[i]; x < x2[i]; x++)
+                for(int y = y1[i]; y < y2[i]; y++)
+                    checked[x][y] = i + 1;
 
-            x1 = Integer.parseInt(st.nextToken()) + OFFSET;
-            y1 = Integer.parseInt(st.nextToken()) + OFFSET;
-            x2 = Integer.parseInt(st.nextToken()) + OFFSET;
-            y2 = Integer.parseInt(st.nextToken()) + OFFSET;
+        int minX = MAX_R, maxX = 0, minY = MAX_R, maxY = 0;
+        boolean firstRectExist = false;
 
-            for(int x = x1; x < x2; x++){
-                for(int y = y1; y < y2; y++){
-                    if(i == 0)
-                        answer[x][y] = 1;
-                    else
-                        answer[x][y] = 2;
+        for(int x = 0; x <= MAX_R; x++)
+            for(int y = 0; y <= MAX_R; y++)
+                if(checked[x][y] == 1) {
+                    firstRectExist = true;
+                    minX = Math.min(minX, x);
+                    maxX = Math.max(maxX, x);
+                    minY = Math.min(minY, y);
+                    maxY = Math.max(maxY, y);
                 }
-            }
-        }
-        int xLen = 0;
-        int yLen = 0;
+        int area;
+        // Case 1. 첫 번째 직사각형이 전혀 남아있지 않다면 넓이는 0입니다.
+        if(!firstRectExist)
+            area = 0;
+        // Case 2. 첫 번째 직사각형이 남아있다면, 넓이를 계산합니다.
+        else
+            area = (maxX + 1  - minX) * (maxY + 1 - minY);
 
-        for(int x = 0; x < 2001; x++){
-            int maxXLen = 0;
-            for(int y = 0; y < 2001; y++){
-              if(answer[x][y] == 1)
-                maxXLen += 1;
-            }
-            xLen = Math.max(maxXLen, xLen); 
-        }
+        System.out.print(area);
+        else
+            area = (maxX + 1  - minX) * (maxY + 1 - minY);
 
-        for(int y = 0; y < 2001; y++){
-            int maxYLen = 0;
-            for(int x = 0; x < 2001; x++){
-              if(answer[x][y] == 1)
-                maxYLen += 1;
-            }
-            yLen = Math.max(maxYLen, yLen); 
-        }
+        System.out.print(area);
+    }
+}
 
-        System.out.print(xLen * yLen);
 
+닫기
+
+0 정답
+
+0 오답
     }
 }
