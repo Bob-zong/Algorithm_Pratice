01999c99287101809245cfaee2c0f3c346a279a3
Park Jun Yong <wns0882@naver.com>
2025-03-19 16:13:59 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250319/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        int num3 = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                for(int k = 1; k <= n; k++)
+                    if((i - num1) <= 2 || (j - num2) <= 2 || (k - num3) <= 2)
+                        cnt += 1;
+
+        System.out.print(cnt);  
+    }
+}
