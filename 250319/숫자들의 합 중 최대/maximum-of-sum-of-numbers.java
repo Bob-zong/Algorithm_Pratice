6be0f19acb699bca7a80b7155c8f786593d4be99
Park Jun Yong <wns0882@naver.com>
2025-03-19 17:53:59 +0900

    Title: 숫자들의 합 중 최대, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

--- 250319/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
+++ 250319/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
@@ -15,11 +15,11 @@ public class Main {
             int sum = 0;
 
             int d1 = i / 10000;
-            int d2 = (i / 1000) % 100;
+            int d2 = (i / 1000) % 10;
             int d3 = (i / 100) % 10;
-            int d4 = (i / 10);
+            int d4 = (i / 10) % 10;
             int d5 = i % 10; 
-            // System.out.printf("%d %d %d %d %d", d1, d2,d3,d4,d5);
+            // System.out.printf("%d %d %d %d %d \n", d1, d2,d3,d4,d5);
             sum = d1 + d2 + d3 + d4 + d5;
             ans = Math.max(ans, sum);
         }
