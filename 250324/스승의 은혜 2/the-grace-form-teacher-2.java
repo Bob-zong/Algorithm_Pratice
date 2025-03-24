ab51013831a020e93156821c905c9031dbd84a3f
Park Jun Yong <wns0882@naver.com>
2025-03-24 17:07:39 +0900

    Title: 스승의 은혜 2, Time: 97ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250324/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int money = Integer.parseInt(st.nextToken());
+
+        int stuCost[] = new int[n];
+        int max = 0;
+
+        for(int i = 0; i < n; i++){
+            stuCost[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0; i < n; i++){
+            stuCost[i] /= stuCost[i] / 2;
+            int sum = 0;
+            int stuNum = 0;
+
+            for(int j = 0; j < n; j++){
+                if(sum <= money)
+                    sum += stuCost[j];
+                
+                if(sum > money)
+                    stuNum = j;
+            }
+            stuCost[i] /= stuCost[i] * 2;
+            max = Math.max(max,stuNum);
+        }
+        
+        System.out.print(max);
+    }
+}
