8fc9ecc15175399a3640c2f31b80bd2dec8c89cd
Park Jun Yong <wns0882@naver.com>
2025-04-04 15:43:52 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250404/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 250404/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -1,17 +1,11 @@
 import java.util.*;
 import java.io.*;
 
-// 1. 바라보는 방향에 벽이 있는지 체크 있으면 -> 방향 반시계 방향으로 전환, 없으면 직진
-// 2. 이동이 가능하면 한칸 전진 후 시계 방향으로 방향 전환 후 한칸 이동
-// 다시 1번부터반복 한칸 전진 할 때 레인지를 벗어나면 통과
-// visited 배열 사용해서 계속 도는 곳을 돌면 -1 출력
-
 public class Main {
     public static int n;
     public static int time;
-
-    public static int answer[][];
-    public static int visited[][];
+    public static int[][] answer;
+    public static int[][] visited;
 
     public static int startX, startY;
     public static int dirNum = 0;
@@ -19,22 +13,19 @@ public class Main {
     public static int[] dx = new int[]{0, -1, 0, 1}; // 오른쪽 위 왼쪽 아래
     public static int[] dy = new int[]{1, 0, -1 ,0};
 
-    public static boolean inRange(int x, int y){
-        return ( 1 <= x && x <= n && 1 <= y && y <= n);
+    public static boolean inRange(int x, int y) {
+        return (1 <= x && x <= n && 1 <= y && y <= n);
     }
 
-    public static boolean checkWall(int x, int y){
-        if(answer[x][y] == 1)
-            return true;
-        else
-            return false;
+    public static boolean checkWall(int x, int y) {
+        return answer[x][y] == 1;
     }
 
-    public static void changeDir(int type){
-        if(type == 0)
-            dirNum = (dirNum + 1) % 4; //반시계 방향
+    public static void changeDir(int type) {
+        if (type == 0)
+            dirNum = (dirNum + 1) % 4; // 반시계 방향
         else  
-            dirNum = (dirNum + 3) % 4; //시계방향 전환
+            dirNum = (dirNum + 3) % 4; // 시계방향 전환
     }
 
     public static void simulate() {
@@ -42,60 +33,72 @@ public class Main {
         int x = startX;
         int y = startY;
         visited[x][y] = 1;
+        int loopCheck = 0;  // 무한 루프 방지를 위한 카운트
 
-        while(true) {
+        while (true) {
             int nx = x + dx[dirNum];
             int ny = y + dy[dirNum];
 
-            if(!inRange(nx, ny)){
+            if (!inRange(nx, ny)) { // 범위를 벗어나면 종료
                 time += 1;
                 break;
             }
-            
-            if(visited[nx][ny] >= 2){
+
+            if (visited[nx][ny] >= 2) { // 같은 곳을 여러 번 방문하면 종료
                 time = -1;
                 break;
             }
-            
 
-            if(checkWall(nx, ny)){
+            if (checkWall(nx, ny)) { // 벽이 있을 경우 방향을 반시계로 변경 후 다시 체크
                 changeDir(0);
-                continue;
-            }else{
-                if(checkWall(nx+dx[(dirNum+3) % 4], ny + dy[(dirNum+3) % 4])){
-                    x = nx;
-                    y = ny;
-                    time += 1;
-                    visited[x][y] += 1;
-                    continue;
-                }else{
-                    changeDir(1);
+                loopCheck++;  // 회전 횟수 증가
+
+                if (loopCheck >= 4) { // 4번 이상 방향 전환만 하면 더 이상 이동 불가능
+                    time = -1;
+                    break;
                 }
+                continue;
             }
 
+            // 벽이 없으면 이동
             x = nx;
             y = ny;
             time += 1;
             visited[x][y] += 1;
+            loopCheck = 0;  // 이동을 하면 회전 카운트 초기화
+
+            // 이동 후, 현재 바라보는 방향에서 시계 방향으로 변경 후 이동할 수 있는지 확인
+            int nextDir = (dirNum + 3) % 4;
+            int nextX = x + dx[nextDir];
+            int nextY = y + dy[nextDir];
+
+            if (!checkWall(nextX, nextY) && inRange(nextX, nextY)) {
+                changeDir(1);  // 시계 방향으로 회전 후 이동
+                x = nextX;
+                y = nextY;
+                time += 1;
+                visited[x][y] += 1;
+            }
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
 
-        answer = new int[n+1][n+1];
-        visited = new int[n+1][n+1];
+        answer = new int[n + 1][n + 1];
+        visited = new int[n + 1][n + 1];
 
         st = new StringTokenizer(br.readLine());
         startX = Integer.parseInt(st.nextToken());
         startY = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             String str = br.readLine();
-            for(int j = 1; j <= n; j++){
-                if(str.charAt(j-1) == '#')
+            for (int j = 1; j <= n; j++) {
+                if (str.charAt(j - 1) == '#')
                     answer[i][j] = 1;
                 else
                     answer[i][j] = 0;
@@ -105,6 +108,5 @@ public class Main {
         simulate();
 
         System.out.print(time);
-
     }
 }
