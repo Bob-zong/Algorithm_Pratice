4311bc45d14e4a97d73f91729909bf7e90e1c423
Park Jun Yong <wns0882@naver.com>
2025-04-08 20:15:24 +0900

    Title: 기울어진 직사각형의 회전, Time: 109ms, Memory: 10MB, Status: Passed - Codetree

--- 250408/기울어진 직사각형의 회전/rotate-slanted-rectangle.java
+++ 250408/기울어진 직사각형의 회전/rotate-slanted-rectangle.java
@@ -8,9 +8,65 @@ public class Main {
     public static int dir;
     public static int row, col;
 
+    public static int[] dx = new int[]{-1, -1, 1, 1};
+    public static int[] dy = new int[]{1, -1, -1, 1};
+
+    public static void simulate() {
+        int[] moveScale = new int[]{m1, m2, m3, m4};
+        int scale = m1 + m2 + m3 + m4;
+        int idx = 0;
+        int[] rotate = new int[scale];
+        // 1번 방향 시계 방향
+        int x = row;
+        int y = col;
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < moveScale[i]; j++){
+                x = x + dx[i];
+                y = y + dy[i];
+
+                rotate[idx++] = answer[x][y]; 
+            }
+        }
+        // 2 3 4 2 3 1 -> 오른쪽 이동 반시계
+        // 2 3 4 2 3 1 -> 왼쪽 이동
+         
+        if(dir == 0){
+            int temp = rotate[idx-1];
+
+            for(int i = idx-1; i > 0; i--){
+                rotate[i] = rotate[i-1];
+            }    
+            
+            rotate[0] = temp;
+        }
+        else{
+            int temp = rotate[0];
+            
+            for(int i = 0; i < idx-1; i++){
+                rotate[i] = rotate[i+1]; 
+            }
+            rotate[idx-1] = temp;
+        }
+
+        x = row;
+        y = col;
+        idx = 0;
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < moveScale[i]; j++){
+                x = x + dx[i];
+                y = y + dy[i];
+
+                answer[x][y] = rotate[idx++]; 
+            }
+        }
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(br.readLine());
 
@@ -30,6 +86,19 @@ public class Main {
         m1 = Integer.parseInt(st.nextToken());
         m2 = Integer.parseInt(st.nextToken());
         m3 = Integer.parseInt(st.nextToken());
+        m4 = Integer.parseInt(st.nextToken());
+        dir = Integer.parseInt(st.nextToken());
+
+        simulate();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                sb.append(answer[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
 
     }
 }
