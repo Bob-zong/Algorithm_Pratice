2da0e49c9a4febecd935f553f7b9d7de0596b03e
Park Jun Yong <wns0882@naver.com>
2025-04-12 16:49:40 +0900

    Title: 밭의 높이를 고르게하기, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250412/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    // 1 ~ n까지 돌면서 각 인덱스에서 T만큼을 H로 만들기 위해 비용 계산
+    // 최소값일 때 갱신
+    // n번 돌고 -> O(n)
+    // idx ~ idx + t 만큼 계산 O(T)
+    // 
+    public static int n, h, t;
+    public static int[] answer;
+    public static int minCost;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+
+        answer = new int[n];
+        minCost = Integer.MAX_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+        int len = st.countTokens();
+        // System.out.print(len);
+        for(int i = 0; i < len; i++){
+            answer[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < len - t; i++){
+            int cost = 0;
+            for(int j = i; j < i + t; j++){
+                cost += Math.abs(answer[j] - h);
+                
+            }
+            minCost = Math.min(minCost, cost);
+        }
+
+        System.out.print(minCost);
+
+    }
+}
