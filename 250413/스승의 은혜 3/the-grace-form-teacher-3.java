d58b40a1c298cd5b7fc7745a8947d17fea18b662
Park Jun Yong <wns0882@naver.com>
2025-04-13 21:29:07 +0900

    Title: 스승의 은혜 3, Time: 229ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 250413/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    // 1 ~ n 까지 반값 쿠폰 적용할 친구를 선택  o(n)
+    // 1 ~ n 까지 총 비용을 계산하는 배열 생성 o(n)
+
+    // 오름차순으로 정렬해(2n^2)
+    // 
+    public static int n, b;
+    public static int[] giftCost;
+    public static int[] deliveryCost;
+
+    public static int getCnt(int discountIdx) {
+        int[] totalCost = new int[n];
+        giftCost[discountIdx] /= 2;
+        for(int i = 0; i < n; i++){
+            totalCost[i] = giftCost[i] + deliveryCost[i];
+        }
+
+        Arrays.sort(totalCost);
+
+        int sum = 0;
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            sum += totalCost[i];
+            if(sum > b)
+                return cnt;
+            
+            cnt += 1;
+        }
+
+        return cnt;
+        
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int maxCnt = 0;
+
+        n = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        giftCost = new int[n];
+        deliveryCost = new int[n];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            giftCost[i] = Integer.parseInt(st.nextToken());
+            deliveryCost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n; i++){
+            maxCnt = Math.max(maxCnt, getCnt(i));
+        }
+        
+        System.out.print(maxCnt);
+
+    }
+}
