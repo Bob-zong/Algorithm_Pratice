c1b44102cfca9197984f2c979bcc11310af2a1a5
Park Jun Yong <wns0882@naver.com>
2025-04-23 20:37:38 +0900

    Title: 두 선분, Time: 96ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250423/두 선분/two-lines.java
+++ 250423/두 선분/two-lines.java
@@ -8,8 +8,29 @@ public class Main {
 
         int x1, x2, x3, x4;
 
-        x1 = Intger.parseInt(st.nextToken());
-                x1 = Intger.parseInt(st.nextToken());
+        x1 = Integer.parseInt(st.nextToken());
+        x2 = Integer.parseInt(st.nextToken());
+        x3 = Integer.parseInt(st.nextToken());
+        x4 = Integer.parseInt(st.nextToken());
 
+        boolean condition = false;
+
+        int sec1 = Math.max(x1, x2);
+        int sec2 = Math.max(x3, x4);
+        
+        if(sec1 > sec2)
+            if(x1 <= x4)
+            condition = true;
+        // x1 ~ x2가 더 큰 경우
+        if(sec2 > sec1)
+            if(x3 <= x2)
+            condition = true;
+        // x3 ~ x4가 더 큰 경우
+        
+        if(condition)
+            System.out.print("intersecting");
+        else
+            System.out.print("nonintersecting");
+        
     }
 }
