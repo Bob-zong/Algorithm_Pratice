a4e190a0138791aa1424d9772b6670640011112d
Park Jun Yong <wns0882@naver.com>
2025-04-23 20:31:37 +0900

    Title: 두 선분, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250423/두 선분/two-lines.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x1, x2, x3, x4;
+
+        x1 = Intger.parseInt(st.nextToken());
+                x1 = Intger.parseInt(st.nextToken());
+
+    }
+}
