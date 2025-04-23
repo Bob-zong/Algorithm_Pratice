cf56d64414e09d39aa7b7b423191bc24ab83b73e
Park Jun Yong <wns0882@naver.com>
2025-04-23 17:14:05 +0900

    Title: 훌륭한 점프, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

--- 250423/훌륭한 점프/great-jump.java
+++ 250423/훌륭한 점프/great-jump.java
@@ -2,23 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static final int MAX_NUM = 100;
+
     public static int n, k;
     public static int[] answer;
-    public static boolean isPossible(int minVal) {
-        int []availableIndices = new int[n];
-        int cnt = 0;
+    public static boolean isPossible(int limit) {
+        int lastIdx = 0;
 
-        for(int i = 0; i < n; i++)
-            if(answer[i] >= minVal)
-                availableIndices[cnt++] = i;
-
-        for(int i = 1; i < cnt; i++) {
-            int dist = availableIndices[i] - availableIndices[i - 1];
-            if(dist > k)
-                return false;
-        }
-
+        for(int i = 1; i < n; i++)
+            if(answer[i] <= limit) {
+                if(i - lastIdx > k)
+                    return false;
+                lastIdx = i;
+            }
         return true;
+
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,21 +26,22 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        answer = new int[n];
+        answer = new int[MAX_N];
 
         st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < n; i++)
             answer[i] = Integer.parseInt(st.nextToken());
 
-        int maximin = 0;
-        
-        for(int a = 0; a < n; a++){
-            if(isPossible(a))
-                maximin = Math.max(maximin,a);
+
+        for(int i = Math.max(answer[0], answer[n-1]); i <= MAX_NUM; i++){
+            if(isPossible(i)) {
+                System.out.print(i);
+                break;
+            }
         }
 
-        System.out.print(maximin);
+        
         
     }
 }
