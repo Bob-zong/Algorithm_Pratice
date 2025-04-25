f6ee7bcbfd02ea76ed58f178add3c53bd0f33ab8
Park Jun Yong <wns0882@naver.com>
2025-04-25 16:17:58 +0900

    Title: 달리자, Time: 96ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250424/달리자/run-and-run.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] A = new int[n];
+        int[] B = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long answer = 0;
+        long diffSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            diffSum += (A[i] - B[i]);
+            answer += Math.abs(diffSum);
+        }
+
+        System.out.println(answer);
+    }
+}
