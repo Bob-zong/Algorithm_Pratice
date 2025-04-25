ada6de333251d9a54f7506a621f42e9e42469ce1
Park Jun Yong <wns0882@naver.com>
2025-04-25 14:58:18 +0900

    Title: 전부 겹치는 선분 2, Time: 106ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250424/전부 겹치는 선분 2/overlapping-line-segments-2.java
+++ 250424/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -8,45 +8,50 @@ class Pair {
         this.x2 = x2;
     }
 }
-
 public class Main {
     public static int n;
+    public static int x1, x2;
     public static Pair[] answer;
+
+    public static int maxX, minX;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
-        answer = new Pair[n];
-
-        for (int i = 0; i < n; i++) {
+        answer = new Pair[n];  // ✅ 여기서 초기화 필요!
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
+
             answer[i] = new Pair(x1, x2);
         }
 
         boolean condition = false;
+        maxX = 0;
+        minX = 0;
 
-        for (int i = 0; i < n; i++) {
-            int maxX = Integer.MIN_VALUE;
-            int minX = Integer.MAX_VALUE;
+        
 
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                if(i == j)
+                    continue;
                 Pair dot = answer[j];
-                maxX = Math.max(maxX, dot.x1);
-                minX = Math.min(minX, dot.x2);
+                minX = Math.min(minX,dot.x2);
+                maxX = Math.max(maxX, dot.x1);    
             }
-
-            if (minX >= maxX) {
+            if(minX < maxX)
                 condition = true;
-                break;  // 하나라도 가능하면 바로 종료
-            }
         }
 
-        System.out.println(condition ? "Yes" : "No");
+        if(condition)
+            System.out.print("Yes");
+        else{
+            System.out.print("No");
+        }
+
     }
 }
