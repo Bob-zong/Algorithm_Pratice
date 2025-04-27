7a4799b4c0a7c39e63c68a20f0e23b9f864fc47f
Park Jun Yong <wns0882@naver.com>
2025-04-27 16:00:29 +0900

    Title: 독서실의 거리두기 2, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250427/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    // 그냥 간격이 제일 넓은 곳 찾아서 그 중앙값에 넣으면 되는 거 아님? -- ㄴㄴ 아님
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int len = str.length();
+        int answer[] = new int[len];
+
+        for(int i = 0; i < n; i++){
+            answer[i] = Character.getNumericValue(str.charAt(i));
+        }
+
+        int maxLen = 0;
+        for(int i = 0; i < len; i++){
+            int minLen = Integer.MAX_VALUE;
+            if(answer[i] == 0) {
+                answer[i] = 1;
+            }else continue;
+            int curIdx = 0;
+            for(int j = 0; j < len; j++){
+                if(answer[i] == 1){
+                    curIdx = j;
+                    break;
+                }
+            }
+            
+            for(int j = curIdx + 1; j < len; j++){
+                if(answer[j] == 1) {
+                    minLen = Math.min(minLen, j-curIdx);
+                    curIdx = j;
+                    
+                    // System.out.print("i : " + i + " " + minLen + " ");
+                }
+            }
+            // System.out.println();
+            maxLen = Math.max(maxLen, minLen);
+            answer[i] = 0;
+        }
+        
+        System.out.print(maxLen);
+    }
+}
