37205b02bb1a9862eac420c10487b2ce29ead8b2
Park Jun Yong <wns0882@naver.com>
2025-04-27 17:47:17 +0900

    Title: L, R 그리고 B, Time: 95ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250427/L, R 그리고 B/l-r-and-b.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        char[][] answer = new char[10][10];
+        int Lrow = 0, Lcol = 0, Rrow = 0, Rcol = 0, Brow = 0, Bcol = 0;
+
+        for(int i = 0; i < 10; i++){
+            String str = br.readLine();
+            for(int j = 0; j < 10; j++){
+                answer[i][j] = str.charAt(j);
+                if(answer[i][j] == 'B'){
+                    Brow = i;
+                    Bcol = j;
+                }
+                if(answer[i][j] == 'R'){
+                    Rrow = i;
+                    Rcol = j;
+                }
+
+                if(answer[i][j] == 'L'){
+                    Lrow = i;
+                    Lcol = j;
+                }
+            }
+        }        
+        int len = 0;
+        // System.out.printf("%d %d %d %d %d %d",Lrow , Lcol , Rrow , Rcol , Brow , Bcol);
+        if((Brow == Rrow && Rrow == Lrow) || (Bcol == Rcol && Rcol == Lcol)){
+            len = Math.abs(Lrow-Brow) + Math.abs(Lcol-Bcol) - 1;
+            len += 2;     
+        }else{
+            len = Math.abs(Lrow-Brow) + Math.abs(Lcol-Bcol) - 1;
+        }
+        System.out.print(len);
+    }
+}
