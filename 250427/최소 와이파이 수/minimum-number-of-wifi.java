c3ac1738cc835601df8002deb347b4f9c6528f7c
Park Jun Yong <wns0882@naver.com>
2025-04-27 19:34:09 +0900

    Title: 최소 와이파이 수, Time: 82ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250427/최소 와이파이 수/minimum-number-of-wifi.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] answer = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            answer[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            if(answer[i] == 1){
+                i = i + m + 1;
+                cnt += 1;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
