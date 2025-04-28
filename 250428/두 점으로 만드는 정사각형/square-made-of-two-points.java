1fbb63dfcd8778f7ce1ab5324b404737907fc447
Park Jun Yong <wns0882@naver.com>
2025-04-28 17:31:31 +0900

    Title: 두 점으로 만드는 정사각형, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

--- 250428/두 점으로 만드는 정사각형/square-made-of-two-points.java
+++ 250428/두 점으로 만드는 정사각형/square-made-of-two-points.java
@@ -10,6 +10,22 @@ public class Main {
         int a1, b1, a2, b2;
 
         x1 = Integer.parseInt(st.nextToken());
-        x1 = Integer.parseInt(st.nextToken());
+        y1 = Integer.parseInt(st.nextToken());
+        x2 = Integer.parseInt(st.nextToken());
+        y2 = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+
+        a1 = Integer.parseInt(st.nextToken());
+        b1 = Integer.parseInt(st.nextToken());
+        a2 = Integer.parseInt(st.nextToken());
+        b2 = Integer.parseInt(st.nextToken());
+
+        int width = Math.max(x2, a2) - Math.min(x1, a1);
+        int height = Math.max(y2, b2) - Math.min(y1, b1);
+
+        int len = Math.max(width, height);
+
+        System.out.print(len * len);
     }
 }
