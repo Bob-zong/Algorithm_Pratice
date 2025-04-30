39337d69dd6f27b287e8a4bfac9d90e54259f8a4
Park Jun Yong <wns0882@naver.com>
2025-04-30 18:59:14 +0900

    Title: 대칭 차집합, Time: 638ms, Memory: 46MB, Status: Passed - Codetree

---
+++ 250430/대칭 차집합/symmetric-difference-set.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int a, b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        int[] aArr = new int[a];
+        int[] bArr = new int[b];
+        HashSet<Integer> aSet = new HashSet<>();
+        HashSet<Integer> bSet = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < a; i++){
+            aArr[i] = Integer.parseInt(st.nextToken());
+            aSet.add(aArr[i]);
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < b; i++){
+            bArr[i] = Integer.parseInt(st.nextToken());
+            bSet.add(bArr[i]);
+        }
+
+
+        for(int i = 0; i < b; i++) {
+            aSet.remove(bArr[i]);
+        }
+        
+        for(int i = 0; i < a; i++){
+            bSet.remove(aArr[i]);
+        }
+
+        System.out.print(aSet.size() + bSet.size());
+
+
+        
+    }
+}
