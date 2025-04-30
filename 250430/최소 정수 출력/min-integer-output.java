d553a2ca1d70f3f73bb7368995b02147eaf88b42
Park Jun Yong <wns0882@naver.com>
2025-04-30 19:39:09 +0900

    Title: 최소 정수 출력, Time: 874ms, Memory: 34MB, Status: Runtime Error - Codetree

--- 250430/최소 정수 출력/min-integer-output.java
+++ 250430/최소 정수 출력/min-integer-output.java
@@ -1,25 +1,40 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.PriorityQueue;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+public class Main {    
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();  
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-        int n = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < n; i++){
-            int num = Integer.parseInt(br.readLine());
-            if(num == 0)
+        // priority queue를 이용하여 진행합니다.
+        for(int i = 0; i < n; i++) {
+            // 0이 아니라면,
+            // 해당 숫자를 priority queue에 넣어줍니다.
+            if(arr[i] != 0)
+                pq.add(arr[i]);
+            // 0이라면
+            // 가장 작은 값을 출력하고
+            // 배열에서 제거합니다.
+            else {
+                // 배열이 비어있다면 0을 출력합니다.
                 if(pq.isEmpty())
-                    sb.append(0).append("\n");
-                else
-                    sb.append(pq.poll()).append("\n");
-            else
-                pq.add(num);
+                    System.out.println(0);
+                // 그렇지 않다면 최솟값을 제거합니다.
+                else {
+                    int minVal = pq.poll();
+                    System.out.println(minVal);
+                }
+            }
         }
-        System.out.print(sb);
     }
 }
