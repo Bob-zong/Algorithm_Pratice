d9ef50ed45fbcbef3eb343cfba7fbfcdc10ecff5
Park Jun Yong <wns0882@naver.com>
2025-05-01 21:14:32 +0900

    Title: 앞에서부터 삭제하기 2, Time: 1819ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

---
+++ 250501/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // k -> 1 ~ n-2
+
+        // 배열에 값으 다 넣어. O(n)
+        // 1 ~ k까지 배열을 순회하면서 q에 넣고 O(k)
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] answer = new int[n+1];
+
+        for(int i = 1; i <= n; i++)
+            answer[i] = Integer.parseInt(st.nextToken());
+
+        float maxAvg = 0;
+
+        for(int k = 1; k <= n-2; k++){
+            int sum = 0;
+            pq.clear();
+            for(int i = k+1; i <= n; i++){
+                int val = answer[i];
+                sum += val;
+                pq.add(val);
+            }
+            sum -= pq.poll();
+            maxAvg = Math.max(maxAvg, (float)(sum / pq.size()));
+        }
+        
+        System.out.printf("%.2f", maxAvg);
+    }
+}
