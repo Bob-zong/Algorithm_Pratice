7f9f642f1e80f494a0de678fe467b3e4f872c95f
Park Jun Yong <wns0882@naver.com>
2025-05-02 16:35:47 +0900

    Title: top K 숫자, Time: 350ms, Memory: 22MB, Status: Passed - Codetree

--- 250502/top K 숫자/top-k-elements.java
+++ 250502/top K 숫자/top-k-elements.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        TreeSet<Integer> s = new TreeSet<>((a,b) -> b - a);
+        TreeSet<Integer> s = new TreeSet<>();
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
@@ -14,15 +14,16 @@ public class Main {
         st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < n; i++) {
-            s.add(Integer.parseInt(st.nextToken()));
+            s.add(-1 * Integer.parseInt(st.nextToken()));
         }
         int cnt = 0;
         for(int i : s){
             if(cnt == k)
                 break;
             cnt++;
-            sb.append(i).append(" ");
+            sb.append(-i).append(" ");
         }
         System.out.print(sb);
     }
 }
+
