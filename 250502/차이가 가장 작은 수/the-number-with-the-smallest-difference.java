a12757c022124b83ae13013baf7ad50793614fc0
Park Jun Yong <wns0882@naver.com>
2025-05-02 18:29:04 +0900

    Title: 차이가 가장 작은 수, Time: 748ms, Memory: 25MB, Status: Passed - Codetree

--- 250502/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 250502/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int ans = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -9,33 +10,27 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
+        int[] arr = new int[n];
         int minDiff = Integer.MAX_VALUE;
 
-        for(int i = 0; i < n; i++){
-            int num = Integer.parseInt(br.readLine());
-
-            
-            Integer nextValue = s.ceiling(num-m);
-            Integer prevValue = s.floor(num+m);
-
-            if(nextValue != null) {
-                int diff = Math.abs(nextValue - num);
-                if(diff < minDiff && diff >= m)
-                    minDiff = diff; 
-            }
-
-            if(prevValue != null) {
-                int diff = Math.abs(prevValue - num);
-                // System.out.println(diff);
-                if(diff < minDiff && diff >= m)
-                    minDiff = diff; 
-            }
-            s.add(num);
-            
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            s.add(arr[i]);
         }
-        if(minDiff != Integer.MAX_VALUE)
-            System.out.print(minDiff);
+        
+        // | x - r | >= m
+        // x - r >= m -> r <= x - m 일 때 최대 값 r 을 구하는 경우
+        // r - x >= m -> r >= m + x
+
+        for(int i = 0; i < n; i++) {
+            int x = arr[i];
+            if(s.ceiling(m+x) != null)
+                ans = Math.min(ans, s.ceiling(m + x) - x);
+            if(s.floor(x - m) != null)
+                ans = Math.min(ans, x - s.floor(x - m));
+        }
+        if(ans != Integer.MAX_VALUE)
+            System.out.print(ans);
         else
             System.out.print(-1);
         
