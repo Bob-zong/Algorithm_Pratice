c54433f9bb4095f8b72b15175a6345008f9bd7eb
Park Jun Yong <wns0882@naver.com>
2025-05-05 20:26:17 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 375ms, Memory: 29MB, Status: Runtime Error - Codetree

---
+++ 250505/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_NUM = 1000000;
+    public static int n, q;
+    public static int[] answer;
+    public static int[] prefixSum;
+
+    public static int getSum(int s, int e) {
+        return ( prefixSum[e] - prefixSum[s-1] );
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        answer = new int[MAX_NUM+1];
+        prefixSum = new int[MAX_NUM+1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i <= n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            answer[num] = 1;
+        }
+
+        for(int i = 1; i <= MAX_NUM; i++) {
+            prefixSum[i] = prefixSum[i-1] + answer[i];
+        }
+
+        for(int i = 1; i <= q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            
+            int cnt = getSum(s, e);
+            sb.append(cnt).append("\n");
+
+        }
+
+        System.out.print(sb);
+    }
+}
