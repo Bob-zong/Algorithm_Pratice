98472da9fa14614a38d88eb683b260b233663473
Park Jun Yong <wns0882@naver.com>
2025-05-10 18:51:35 +0900

    Title: 호텔 예약, Time: 99ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250510/호텔 예약/reserve-hotel.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+class Point implements Comparable<Point> {
+    int x, v, index;
+
+    public Point(int x, int v, int index) {
+        this.x = x;
+        this.v = v;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(Point p){
+        return this.x - p.x;
+    }
+        
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        ArrayList<Point> points = new ArrayList<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int x1, x2;
+
+            x1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+
+            points.add(new Point(x1, +1, i));
+            points.add(new Point(x2, -1, i));
+        }
+        int ans = 0;
+        int sum = 0;
+        
+        Collections.sort(points);
+        for(int i = 0; i < 2 * n; i++){
+            int x = points.get(i).x;
+            int v = points.get(i).v;
+
+            sum += v;
+            ans = Math.max(ans, sum);
+            
+            
+
+
+        }
+
+        System.out.print(ans);
+    }
+}
