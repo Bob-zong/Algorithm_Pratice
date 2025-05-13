5e349361dd4b82278adecf37c174e3b65500e025
Park Jun Yong <wns0882@naver.com>
2025-05-13 21:33:23 +0900

    Title: 가장 짧은 부분합, Time: 262ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250513/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, s;
+    public static final int MAX_NUM = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        int[] answer = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i <= n; i++)
+            answer[i] = Integer.parseInt(st.nextToken());
+
+        int ans = MAX_NUM;
+        int sumVal = 0;
+
+        int j = 1;
+
+        for(int i = 1; i <= n; i++) {
+                
+            while(j <= n && sumVal + answer[j] < s) {
+                sumVal += answer[j];
+                j++;
+            }
+            
+            ans = Math.min(ans, j-i+1);
+            
+
+            sumVal -= answer[i];
+        }
+
+        if(ans == MAX_NUM)
+            System.out.print(-1);
+        else System.out.print(ans);
+        
+
+        // Please write your code here.
+    }
+}
