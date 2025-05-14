926d4dc93c4df20a8680e6a7fafdb9e003ef06e5
Park Jun Yong <wns0882@naver.com>
2025-05-14 18:02:38 +0900

    Title: 정수 두 개의 합 2, Time: 252ms, Memory: 15MB, Status: Passed - Codetree

--- 250514/정수 두 개의 합 2/sum-of-two-integers-2.java
+++ 250514/정수 두 개의 합 2/sum-of-two-integers-2.java
@@ -21,25 +21,26 @@ public class Main {
 
         Arrays.sort(answer);
 
-        int j = 1;
+
         int ans = 0;
-        int sumVal = 0;
 
-        for(int i = 1; i <= n; i++){
-            sumVal = answer[i];
 
-            while(j+1 <= n && sumVal + answer[j+1] <= k){
-                j++;
-            }
-            // j = 3; i = 1
-            if(sumVal + answer[j] <= k)
-                ans += (j - i);
+        int j = n;
+        for(int i = 1; i <= n; i++) {
+            // 구간 내 합이 k보다 크면 계속 진행합니다.
+            while(j != 1 && answer[i] + answer[j] > k)
+                j--;
 
-            if(sumVal >= k)
+            // j가 i보다 같거나 작아져야만 두 숫자의 합이 k 이내가 된다면
+            // 더 이상 진행이 의미가 없으므로 종료합니다.
+            if(j <= i)
                 break;
 
-            // System.out.println(ans);
-            sumVal -= answer[i];
+            // 현재 숫자 arr[i]에 대해
+            // [i + 1, j] 내에 있는 모든 숫자가 정확히
+            // arr[i]와의 합이 k 이하가 되는 경우임을
+            // 확신할 수 있으므로 j - i를 답에 더해줍니다.
+            ans += j - i;
         }
         
         System.out.print(ans);
