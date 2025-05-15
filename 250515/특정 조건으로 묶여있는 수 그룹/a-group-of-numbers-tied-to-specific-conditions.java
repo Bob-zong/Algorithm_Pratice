145b951a30f7e1eabe558d16c7449e15d0daecda
Park Jun Yong <wns0882@naver.com>
2025-05-15 15:51:22 +0900

    Title: 특정 조건으로 묶여있는 수 그룹, Time: 201ms, Memory: 13MB, Status: Wrong Answer - Codetree

--- 250515/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.java
+++ 250515/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.java
@@ -1,43 +1,61 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
+    public static final int MAX_N = 50000;
+
+    public static int[] arr = new int[MAX_N + 1];
+    public static int[] L = new int[MAX_N + 1];
+    public static int[] R = new int[MAX_N + 2];
     public static int n, k;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        n = sc.nextInt();
+        k = sc.nextInt();
 
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(answer);
+        Arrays.sort(arr, 1, n + 1);
 
-        int[] dp = new int[n]; // dp[i]: i를 시작점으로 하는 최대 구간 길이
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            while (j < n && answer[j] - answer[i] <= k) {
+        // L[i]: 1 ~ i까지 중에서 오른쪽으로 확장 가능한 최대 구간 길이
+        int j = 1;
+        for (int i = 1; i <= n; i++) {
+            while (j <= n && arr[j] - arr[i] <= k)
                 j++;
-            }
-            dp[i] = j - i;
+
+            int len = j - i;
+            L[i] = len;
         }
 
-        // R[i] = i부터 n까지 중 최대 dp값 (suffix max)
-        int[] R = new int[n + 1];
-        R[n] = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            R[i] = Math.max(R[i + 1], dp[i]);
+        // 누적 최댓값: L[i] ← max(L[1..i])
+        for (int i = 2; i <= n; i++)
+            L[i] = Math.max(L[i], L[i - 1]);
+
+        // R[i]: i ~ n까지 중에서 오른쪽으로 확장 가능한 최대 구간 길이
+        j = 1;
+        for (int i = 1; i <= n; i++) {
+            while (j <= n && arr[j] - arr[i] <= k)
+                j++;
+
+            int len = j - i;
+            R[i] = len;
         }
 
+        // 누적 최댓값: R[i] ← max(R[i..n])
+        for (int i = n - 1; i >= 1; i--)
+            R[i] = Math.max(R[i], R[i + 1]);
+
+        // 두 그룹을 나눠서 최댓값 합 구하기
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, dp[i] + R[i + dp[i]]);
+        for (int i = 1; i < n; i++) {
+            ans = Math.max(ans, L[i] + R[i + 1]);
         }
+
+        // 하나의 그룹만 사용하는 경우도 포함
+        ans = Math.max(ans, L[n]);
 
         System.out.println(ans);
     }
