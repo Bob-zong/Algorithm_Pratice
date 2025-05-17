eb244684cef8e557a7a9b1fe39d501492dc48c2c
Park Jun Yong <wns0882@naver.com>
2025-05-17 19:40:27 +0900

    Title: 화재 진압, Time: 607ms, Memory: 28MB, Status: Passed - Codetree

--- 250517/화재 진압/fire-suppression.java
+++ 250517/화재 진압/fire-suppression.java
@@ -16,37 +16,30 @@ public class Main {
         int[] stationPos = new int[m];
 
         st = new StringTokenizer(br.readLine());
-
         for(int i = 0; i < n; i++)
             firePos[i] = Integer.parseInt(st.nextToken());
-        
+
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++){
             stationPos[i] = Integer.parseInt(st.nextToken());
             s.add(stationPos[i]);
         }
+
         long ans = 0;
 
         for(int i = 0; i < n; i++) {
             Integer x1 = s.ceiling(firePos[i]);
             Integer x2 = s.floor(firePos[i]);
-            long nearPos = Integer.MAX_VALUE;
+            long nearDist = Long.MAX_VALUE;
 
             if(x1 != null)
-                nearPos = Math.min(nearPos, x1);
+                nearDist = Math.min(nearDist, Math.abs(firePos[i] - x1));
             if(x2 != null)
-                nearPos = Math.min(nearPos, x2);
-            
-            // System.out.println(nearPos);
-            // if(x1 != null && x2 != null)
-            //     ans = Math.max(Math.abs(firePos[i]- x1) , Math.abs(firePos[i]- x2));
-            ans = Math.max(ans, Math.abs(firePos[i] - nearPos));
+                nearDist = Math.min(nearDist, Math.abs(firePos[i] - x2));
+
+            ans = Math.max(ans, nearDist);
         }
 
         System.out.print(ans);
-            
-
-        
-
     }
 }
