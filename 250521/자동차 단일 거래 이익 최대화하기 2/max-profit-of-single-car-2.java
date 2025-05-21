94777ea2fec53f2b7d0a51c90059039139c6c9b1
Park Jun Yong <wns0882@naver.com>
2025-05-21 16:22:36 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 175ms, Memory: 14MB, Status: Passed - Codetree

--- 250521/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
+++ 250521/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
@@ -9,36 +9,28 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
-        int[] L = new int[n+1];
-        int[] R = new int[n+1];
+       
+        int[] answer = new int[n];
 
-        int[] answer = new int[n+1];
-
-        for(int i = 1; i <= n; i++)
+        for(int i = 0; i < n; i++)
             answer[i] = Integer.parseInt(st.nextToken());
 
-        L[0] = answer[1];
-        for(int i = 1; i <= n; i++)
-            L[i] = Math.min(L[i-1], answer[i]);
 
-        R[n] = answer[n];
-        for(int i = n-1; i >= 1; i--)
-            R[i] = Math.max(R[i+1], answer[i]);
+        int maxProfit = 0;
 
-
-        int ans = 0;
+        int min = answer[0];
 
         for(int i = 0; i < n; i++){
-            ans = Math.max(ans, R[i+1] - L[i]);
-        }         
-        
-        // for(int i : L)
-        //     System.out.print(i + " ");
-        
-        // System.out.println();
+            int profit = answer[i] - min;
 
-        // for(int i : R)
-        //     System.out.print(i + " ");
-        System.out.print(ans);
+            if(profit > maxProfit)
+                maxProfit = profit;
+            
+            if(min > answer[i])
+                min = answer[i];
+            
+        }
+       
+        System.out.print(maxProfit);
     }
 }
