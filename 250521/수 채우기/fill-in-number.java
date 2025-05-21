1f9c6e2fcff351e6ba4dec3e9bb6334b7e6fbcd6
Park Jun Yong <wns0882@naver.com>
2025-05-21 15:58:11 +0900

    Title: 수 채우기, Time: 81ms, Memory: 9MB, Status: Passed - Codetree

--- 250521/수 채우기/fill-in-number.java
+++ 250521/수 채우기/fill-in-number.java
@@ -4,48 +4,19 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         int n = Integer.parseInt(br.readLine());
-
 
         int ans = -1;
 
-        int fiveCnt = 0;
-        int twoCnt = 0;
-        int remain = 0;
-
-        fiveCnt = n / 5;
-        remain = n % 5;
-// 22 24 26 28
-// 21 23 27 29
-        if(n == 1 || n == 3)
-            ans = -1;
-        
-        else if(remain % 2 != 0 && remain < 5) {
-            if(fiveCnt != 0)
-                fiveCnt--;
-            
-            twoCnt = (n - (5 * fiveCnt)) / 2;
-            ans = twoCnt + fiveCnt;
-        }
-        else if(remain % 2 == 0 && remain > 5){
-            if(fiveCnt != 0)
-                fiveCnt--;
-            
-            twoCnt = (n - (5 * fiveCnt)) / 2;
-            ans = twoCnt + fiveCnt;
-        }
-        else{
-            twoCnt = remain / 2;
-            ans = twoCnt + fiveCnt;
+        for (int fiveCnt = n / 5; fiveCnt >= 0; fiveCnt--) {
+            int remain = n - (fiveCnt * 5);
+            if (remain % 2 == 0) {
+                int twoCnt = remain / 2;
+                ans = fiveCnt + twoCnt;
+                break;
+            }
         }
 
-        
-        System.out.print(ans);
-
-
-
-        
+        System.out.println(ans);
     }
 }
