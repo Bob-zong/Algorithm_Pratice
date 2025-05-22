fcb77bf1c5fea42f3ef561947c89e69936aab42b
Park Jun Yong <wns0882@naver.com>
2025-05-22 23:23:24 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

--- 250522/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
+++ 250522/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
@@ -1,38 +1,32 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n, order;
-    public static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        // 입력 설정
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        long n = Long.parseLong(br.readLine());
+        long k = Long.parseLong(br.readLine());
 
-    public static void binary_search() {
-        int left = 1;
-        int right = n * n;
+        long left = 1, right = n * n, answer = 0;
 
-        while(left <= right) {
-            int mid = (left + right) / 2;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            // mid보다 작거나 같은 수가 몇 개인지 카운트
             long cnt = 0;
-            for(int i = 1; i <= n; i++){
+            for (int i = 1; i <= n; i++) {
                 cnt += Math.min(n, mid / i);
             }
 
-            if(cnt >= order) {
-                    right = mid-1;
-                    answer = mid;
-                }
-            else 
-                left = mid+1;
+            if (cnt >= k) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
 
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-        order = Integer.parseInt(br.readLine());
-        binary_search();
-        System.out.print(answer);
+        System.out.println(answer);
     }
 }
-
-
