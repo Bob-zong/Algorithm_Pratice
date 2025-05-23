671eff358ca61884c99760b16f3c51b858712b48
Park Jun Yong <wns0882@naver.com>
2025-05-23 16:00:29 +0900

    Title: 좌우 반전시키기, Time: 96ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250523/좌우 반전시키기/flip-left-and-right.java
+++ 250523/좌우 반전시키기/flip-left-and-right.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static int n;
     public static int[] answer;
-    public static boolean[] visited;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +15,6 @@ public class Main {
         st = new StringTokenizer(br.readLine());
 
         answer = new int[n+1];
-        visited = new boolean[n+1];
 
         for(int i = 1; i <= n; i++){
             answer[i] = Integer.parseInt(st.nextToken());
@@ -23,21 +22,21 @@ public class Main {
 
         int ans = 0;
 
-        for(int i = 1; i <= n; i++) {
-            if(i == n && answer[n] == 0){
-                ans = -1;
-                break;
-            }
+        int idx = 2;
 
-            if(answer[i] == 1)
-                continue;
-            
-            if(answer[i] == 0){
-                answer[i+1] = (answer[i+1] + 1) % 2;
-                answer[i+2] = (answer[i+2] + 1) % 2;
+        while(idx < n) {
+            if(answer[idx-1] == 0){
+                answer[idx-1] = (answer[idx-1] + 1) % 2;
+                answer[idx] = (answer[idx] + 1) % 2;
+                answer[idx+1] = (answer[idx+1] + 1) % 2;
                 ans++;
             }
+
+            idx++;
         }
+        
+        if(answer[n-1] == 0 || answer[n] == 0)
+            ans = -1;
 
         System.out.print(ans);
     }
