34685a58c66293e22335dc78f766f562828b0be7
Park Jun Yong <wns0882@naver.com>
2025-06-04 12:01:51 +0900

    Title: G & H 반전시키기 3, Time: 94ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250604/G & H 반전시키기 3/reversing-g-and-h-3.java
+++ 250604/G & H 반전시키기 3/reversing-g-and-h-3.java
@@ -6,32 +6,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         n = Integer.parseInt(br.readLine());
 
         String originStr = br.readLine();
         String targetStr = br.readLine();
-        
+
         char[] origin = originStr.toCharArray();
         char[] target = targetStr.toCharArray();
 
-
-        
-        int j = 0;
         int ans = 0;
 
-        for(int i = 0; i < n; i++) {
-            int idx = 0;
-            
-            while((origin[j] != target[j]) && idx < 4 && j < n){
-                j++;
-                idx++;
-            }
-
-            if(idx != 0)
+        for (int i = 0; i < n; i++) {
+            if (origin[i] != target[i]) {
                 ans++;
-            if(j > i)
-                i = j;
+                // 최대 4칸까지 고칠 수 있으므로, i부터 i+3까지 다 고쳐버리자
+                for (int j = i; j < i + 4 && j < n; j++) {
+                    origin[j] = target[j];
+                }
+            }
         }
 
         System.out.print(ans);
