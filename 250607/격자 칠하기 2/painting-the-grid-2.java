c157d4b5f8df63696a66a8b89faa55a3429c2577
Park Jun Yong <wns0882@naver.com>
2025-06-07 16:44:03 +0900

    Title: 격자 칠하기 2, Time: 178ms, Memory: 13MB, Status: Passed - Codetree

--- 250607/격자 칠하기 2/painting-the-grid-2.java
+++ 250607/격자 칠하기 2/painting-the-grid-2.java
@@ -8,8 +8,8 @@ class Point {
         this.x = x;
         this.y = y;
     }
-
 }
+
 public class Main {
     public static final int MAX_NUM = 1000000;
 
@@ -17,91 +17,68 @@ public class Main {
     public static int[][] board;
     public static boolean[][] visited;
 
-    public static int[] dx = new int[]{-1, 0, 1, 0};
-    public static int[] dy = new int[]{0, 1, 0, -1};
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
     public static boolean inRange(int x, int y) {
-        return ( 1 <= x && x <= n && 1 <= y && y <= n );
+        return 1 <= x && x <= n && 1 <= y && y <= n;
     }
 
-    public static boolean canGo(int x, int y, int nx, int ny, int D) {
-        if(!inRange(nx, ny))
-            return false;
-
-        if(visited[nx][ny])
-            return false;
-
-        if(Math.abs(board[nx][ny] - board[x][y]) > D)
-            return false;
-
-        
-        return true;
-    }
-
-    public static void BFS(int mid) {
+    public static int BFS(int startX, int startY, int D) {
         Queue<Point> q = new LinkedList<>();
-        
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(board[i][j] == 0)
-                    q.add(new Point(i, j));
-            }
-        }
+        q.add(new Point(startX, startY));
+        visited[startX][startY] = true;
 
-        while(!q.isEmpty()) {
+        int size = 1;
+
+        while (!q.isEmpty()) {
             Point curr = q.poll();
-            int x, y;
-            
-            x = curr.x;
-            y = curr.y;
-            visited[x][y] = true;
-            
-            for(int i = 0; i < 4; i++){
+            int x = curr.x;
+            int y = curr.y;
+
+            for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if(canGo(x, y, nx, ny, mid)){
-                    q.add(new Point(nx, ny));
+                if (inRange(nx, ny) && !visited[nx][ny]
+                    && Math.abs(board[nx][ny] - board[x][y]) <= D) {
                     visited[nx][ny] = true;
+                    q.add(new Point(nx, ny));
+                    size++;
                 }
             }
         }
+
+        return size;
     }
 
-    public static boolean isPossible(int mid) {
-        visited = new boolean[n+1][n+1];
-        BFS(mid);
-    
-        int cnt = 0;
-        int minValue;
-        
-        if(n % 2 == 0)
-            minValue = (n * n) / 2;
-        else
-            minValue = (n * n) / 2 + 1;
+    public static boolean isPossible(int D) {
+        visited = new boolean[n + 1][n + 1];
+        int maxAreaSize = 0;
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(visited[i][j])
-                    cnt++;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (!visited[i][j]) {
+                    int areaSize = BFS(i, j, D);
+                    maxAreaSize = Math.max(maxAreaSize, areaSize);
+                }
             }
         }
 
-        return cnt >= minValue;
-        
+        int halfOrMore = (n * n + 1) / 2;  // 절반 이상
+        return maxAreaSize >= halfOrMore;
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
+        board = new int[n + 1][n + 1];
 
-        board = new int[n+1][n+1];
-
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            for(int j = 1; j <= n; j++){
+            for (int j = 1; j <= n; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -110,19 +87,17 @@ public class Main {
         int right = MAX_NUM;
         int ans = MAX_NUM;
 
-        while(left <= right) {
+        while (left <= right) {
             int mid = (left + right) / 2;
 
-            if(isPossible(mid)) {
+            if (isPossible(mid)) {
+                ans = mid;
                 right = mid - 1;
-                ans = Math.min(ans, mid);
-            }
-            else {
+            } else {
                 left = mid + 1;
             }
         }
 
         System.out.print(ans);
-
     }
 }
